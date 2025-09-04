510f9454f2c68ddbb5e6d2a9d7a5f9bf2b34f47e
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-04 10:02:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+require('dotenv').config();
+const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsdoc = require('swagger-jsdoc');
 
+const moviesRoutes = require('./src/routes/movies.routes');
+const errorHandler = require('./src/middleware/errorHandler.middleware');
+const loggerMiddleware = require('./src/middleware/logger.middleware');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Swagger setup
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Movie-Match API',
+      version: '1.0.0',
+      description: 'A simple REST API for movie data',
+    },
+    servers: [
+      {
+        url: `http://localhost:${PORT}`,
+      },
+    ],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// Middleware
+app.use(express.json());
+app.use(loggerMiddleware);
+
+// Routes
+app.use('/movies', moviesRoutes);
+
+// Error Handling Middleware
+app.use(errorHandler);
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
