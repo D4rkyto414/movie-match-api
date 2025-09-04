a1d6427c8be0d789e83cec28d4cdc6d0bd556c51
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-04 10:12:55 -0500

    Update movies.controllers.js

--- src/controllers/movies.controllers.js
+++ src/controllers/movies.controllers.js
@@ -1,1 +1,35 @@
+const moviesService = require('../services/movies.services');
 
+exports.getAllMovies = (req, res, next) => {
+  try {
+    const filters = req.query;
+    const result = moviesService.getFilteredMovies(filters);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getMovieById = (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const movie = moviesService.getMovieById(id);
+    if (!movie) {
+      const error = new Error('Movie not found.');
+      error.status = 404;
+      throw error;
+    }
+    res.json(movie);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getGenres = (req, res, next) => {
+  try {
+    const genres = moviesService.getUniqueGenres();
+    res.json(genres);
+  } catch (error) {
+    next(error);
+  }
+};
