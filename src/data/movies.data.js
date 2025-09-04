11b14f827b2347a0722ea59010c805e5ca6d0535
Adriano Paul <adrianopaulperez2005@gmail.com>
2025-09-04 10:11:50 -0500

    Update movies.data.js

--- src/data/movies.data.js
+++ src/data/movies.data.js
@@ -1,1 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require('csv-parser');
 
+const movies = [];
+const filePath = path.join(__dirname, '../../movies.csv');
+
+fs.createReadStream(filePath)
+  .pipe(csv())
+  .on('data', (data) => {
+    // Clean and transform data
+    const movie = {
+      id: data.id,
+      title: data.title,
+      genres: data.genres.split('|'),
+      year: parseInt(data.year),
+      director: data.director,
+      runtime: parseInt(data.runtime),
+    };
+    movies.push(movie);
+  })
+  .on('end', () => {
+    console.log('Movies data loaded.');
+  });
+
+module.exports = movies;
